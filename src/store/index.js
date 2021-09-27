7780688b21d5adf5dc9012a527c609319f1b1e3d
Fatih <fatihy@yahoo.com>
2021-09-27 10:44:13 +0300

    Vuex updated

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,10 @@ export const mutations = {
     state.todos.push(todo)
   },
   REMOVE_TODO(state, todo) {
-    state.todos.splice(state.todos.indexOf(todo), 1)
+    state.todos.splice(state.todos.findIndex((item) => todo.id === item.id), 1)
   },
   UPDATE_TODO(state, todo) {
-    state.todos.splice(state.todos.indexOf(todo), 1, todo)
+    state.todos.splice(state.todos.findIndex((item) => todo.id === item.id), 1, todo)
   },
   CLEAR_TODOS(state) {
     state.todos = []
@@ -20,10 +20,35 @@ export const mutations = {
 
 export default createStore({
   state: {
-    todos: []
+    todos: [
+      {id: 1, text: 'Learn Vue', completed: false},
+      {id: 2, text: 'Learn Vuex', completed: false},
+      {id: 3, text: 'Learn Vue Router', completed: false},
+      {id: 4, text: 'Learn Vuex Router', completed: false},
+      {id: 5, text: 'Learn Vuex Router', completed: false},
+      {id: 6, text: 'Learn Vuex Router', completed: false}
+    ]
   },
   mutations: mutations,
   actions: {
+    addTodo({ commit }, todo) {
+      commit('ADD_TODO', todo)
+    },
+    removeTodo({ commit }, todo) {
+      commit('REMOVE_TODO', todo)
+    },
+    updateTodo({ commit }, todo) {
+      commit('UPDATE_TODO', todo)
+    },
+    clearTodos({ commit }) {
+      commit('CLEAR_TODOS')
+    },
+    fetchTodos({ commit }, todos) {
+      commit('FETCH_TODOS', todos)
+    }
+  },
+  getters: {
+    todos: state => state.todos
   },
   modules: {
   }
